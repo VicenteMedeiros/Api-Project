43e25a923e61474cde1044148bac76e63ff8445b
VicenteMedeiros <vicenteaml@hotmail.com>
2023-05-29 20:12:03 -0300

    Api CEP

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 //Cep via api
 const inputCep = document.getElementById('cep');
 const btnCep = document.getElementById('btnCep');
-const resultadoCep = document.querySelector('.resultadoCep');
+const resultadoCep = document.querySelector('.resultadoCep');btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.text())
+  .then(body => {
+    resultadoCep.innerText = body;
+  })
+}
